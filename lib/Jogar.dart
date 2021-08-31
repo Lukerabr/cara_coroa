5afaca139c772204ae8b2f4727fe8ce579ffc614
Lucca D'Amico Diniz <lucca.damico@hotmail.com>
2021-08-31 18:14:44 -0400

    Finalizada criação da interface.

---
+++ lib/Jogar.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Jogar extends StatefulWidget {
+  const Jogar({Key? key}) : super(key: key);
+
+  @override
+  _JogarState createState() => _JogarState();
+}
+
+class _JogarState extends State<Jogar> {
+
+  void _exibirResultado(){
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff61bd86),
+      //backgroundColor: Color.fromRGBO(255, 204, 128, 1),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.asset("imagens/logo.png"),
+            GestureDetector(
+              onTap: _exibirResultado,
+              child: Image.asset("imagens/botao_jogar.png"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
